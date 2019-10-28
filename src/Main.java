8eb8045f8aace8f542461d679acef7faa562da95
Mi Kyung Han <mhan@calbaptist.edu>
2019-10-28 09:59:50 -0700

    adding main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        Point p1 = new Point(5,3);
+        Point p2 = new Point(5,3);
+        Point p3 = p2;
+
+        System.out.println("p1 == p2 " + (p1 == p2));
+        System.out.println("p1 == p3 " + (p1 == p3));
+        System.out.println("p3 == p2 " + (p3 == p2));
+
+        System.out.println("p1.equals(p2) " + (p1.equals(p2)));
+        System.out.println("p1.equals(p3) " + (p1.equals(p3)));
+        System.out.println("p3.equals(p2) " + (p3.equals(p2)));
     }
 }
